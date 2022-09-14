60e424bc38307aa9064d4758045c33655d9b74ae
Idan Sagi <idansagi@gmail.com>
2022-09-14 17:09:55 +0300

    Linter clean

--- src/features/users/Users.tsx
+++ src/features/users/Users.tsx
@@ -3,28 +3,22 @@ import { IUser } from "./interfaces/user.interface";
 import styles from "./Users.module.css";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { RootState } from "../../app/store";
-import {
-  useFetchUsersByPageQuery,
-  useLazyFetchUsersByPageQuery,
-} from "./usersQuery";
+import { useLazyFetchUsersByPageQuery } from "./usersQuery";
 
 import { appendUsers } from "./usersSlice";
-import { useEffect, useCallback } from "react";
+import { useEffect } from "react";
 
 export const UserPage = () => {
   const page = useAppSelector((state: RootState) => state.users.page);
   const users = useAppSelector((state: RootState) => state.users.users);
   const dispatch = useAppDispatch();
 
-  // const { isFetching, data } = useFetchUsersByPageQuery(page);
   const [trigger, { isFetching }] = useLazyFetchUsersByPageQuery();
 
   async function fetchUsers() {
     const { data } = await trigger(page);
     dispatch(appendUsers(data));
   }
-
-  const memoFetch = useCallback(fetchUsers, []);
 
   useEffect(() => {
     fetchUsers();
